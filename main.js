42a193f4786ec8cdfc04f7f7627332217fd1be15
krishan456 <77932082+krishan456@users.noreply.github.com>
2021-02-02 17:09:46 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+//<p> I believe in God, only I spell it Nature. —Frank Lloyd Wright</p>
+//<p> Nature does not hurry, yet everything is accomplished. —Lao Tzu</p>
+//               <p> Nature does not hurry, yet everything is accomplished. —Lao Tzu</p> <img class="images" src="https://th.bing.com/th/id/OIP.TvJ7UKIVQnt8oNBWcy8RXwHaFF?w=225&h=180&c=7&o=5&dpr=1.1&pid=1.7">
+//  <p> Heaven is under our feet as well as over our heads. —Henry David Thoreau</p>
+//<img class="images" src="https://th.bing.com/th/id/OIP.WNwf6XlXL1pnS3gLx3d3lAHaEo?w=276&h=180&c=7&o=5&dpr=1.1&pid=1.7">
